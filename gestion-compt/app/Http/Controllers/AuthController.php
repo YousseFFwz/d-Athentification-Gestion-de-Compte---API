21703140dff0c71292898e2c2c86803b8b2f1bd5
youssef faouzi <yj1984515@gmail.com>
2026-03-12 08:24:15 +0100

    feat : ajoute methode login

--- gestion-compt/app/Http/Controllers/AuthController.php
+++ gestion-compt/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -25,6 +26,29 @@ class AuthController extends Controller
             'status' => "succes" ,
             'message' => "create user to database" ,
             'data' => $user ,
-        ]);
+        ],201);
+     }
+
+
+     public function login(Request $request){
+         
+      $validate = $request->validate([
+         'email' => 'email|required' ,
+         'password' => 'required|string' ,
+      ]) ;
+
+      
+       if(!Auth::attempt($validate)){
+         return response()->json([
+            'status' => 'erorr' ,
+            'message' => 'email or password field' ,
+         ],422);
+       }
+
+       return response()->json([
+           'status' => 'success' ,
+           'message' => 'login successfully' ,
+           'data' => Auth::user() ,
+       ],200);
      }
 }
--- gestion-compt/routes/api.php
+++ gestion-compt/routes/api.php
@@ -8,4 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-route::post('registre' , [AuthController::class , 'registre']) ;
+route::post('registre' , [AuthController::class , 'registre']);
+route::post('login' , [AuthController::class , 'login']);
